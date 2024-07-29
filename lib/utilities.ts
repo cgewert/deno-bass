bc2fa22545179557ebccaad8ce174e82616450d2
cgewert <cgewert@gmail.com>
2024-07-29 20:34:35 +0200

    Add missing change

--- lib/utilities.ts
+++ lib/utilities.ts
@@ -8,6 +8,8 @@ export const LOWORD = (x: number) => x & 0x0000ffff;
 export const ToCString = (x: string) => new TextEncoder().encode(x + "\0");
 export const UInt8BufferToString = (x: Uint8Array) =>
   Deno.UnsafePointerView.getCString(Deno.UnsafePointer.of(x));
+export const PointerToString = (x: Deno.UnsafePointer) =>
+  Deno.UnsafePointerView.getCString(x);
 
 // Error handling
 export const GetBASSErrorCode = () => {
